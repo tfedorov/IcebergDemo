7e164bf77caf165b0e20ce26c3be759ce119a306
Taras Fedorov <tfedorov@apple.com>
2023-04-20 18:56:55 +0300

    Add iceberg

--- src/test/scala/com/tfedorov/icebergdemo/DemoSpec.scala
+++ src/test/scala/com/tfedorov/icebergdemo/DemoSpec.scala
@@ -1,5 +1,7 @@
 package com.tfedorov.icebergdemo
 
+import com.tfedorov.icebergdemo.SparkLocalExtractor.localSession
+import org.apache.spark.sql.SparkSession
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
@@ -7,6 +9,16 @@ import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 class DemoSpec extends AnyFreeSpec with Matchers {
 
   "2 + 2 spec" in {
+    val spark:SparkSession = localSession
+    spark.sql(
+      """CREATE NAMESPACE IF NOT EXISTS harry"""
+    )
+    spark.sql(
+      """CREATE TABLE IF NOT EXISTS harry.integrated_table (key string, value string)
+        USING iceberg PARTITIONED BY (key)
+        LOCATION 'src/test/resources/iceberg/spark_catalog/dir/harry/integrated_table'"""
+    )
+
     2 + 2 shouldBe 4
   }
 
